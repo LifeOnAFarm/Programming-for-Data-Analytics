e96840a8ea4079a7d6756b4b4995ca55034b3c18
Seamus de Cleir <sdecleir@gmail.com>
2023-10-05 19:20:21 +0100

    Updates
    Updates

--- Week 4/Week4Lab - XML.py
+++ Week 4/Week4Lab - XML.py
@@ -1,30 +1,21 @@
 # Week 4 Lab
 # Seamus de Cleir
 
-import os
+# Import the ElementTree module
+import xml.etree.ElementTree as ET
 
-#1
-# Creates text file and then reads it
-with open("Week4Lab.txt", "w+") as f:
-    f.write("Hello World\nThis is my text file\nIt's stores text etc. etc.\n")
-    f.seek(0)
-    print(f.read())
+# Open file and parse it
+tree = ET.parse('movies.xml')
+# Get the root element and print it
+root = tree.getroot()
+print(root.tag)
 
-#2
-# Appends to text file and then reads it
-with open("Week4Lab.txt", "a+") as f:
-    f.write("Hello World\nThis is my text file\nIt's stores text etc. etc.\n")
-    f.seek(0)
-    print(f.read())
+# Print the child elements
+for child in root:
+    print(child.tag, child.attrib)
 
-#3
-# Reads a text file and then copies it to a new file
-with open("Week4LabNew.txt", "a+") as f:
-    with open("Week4Lab.txt", "r") as f2:
-        f.write(f2.read())
-    f.seek(0)
-    print(f.read())
+# Print the title of each movie
+for movie in root.iter('movie'):
+    print(movie.attrib['title'])
 
-# Removes the text files for debugging
-#os.remove("Week4Lab.txt")
-#os.remove("Week4LabNew.txt")
+[print(elem.tag) for elem in root.iter()]
